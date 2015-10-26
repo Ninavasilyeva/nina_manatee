0c20c7d32f0008b60ee7b1b5b8df61a85d9dd1c6
Nina <nina.vasilyeba625@yandex.com>
2015-10-26 05:06:39 -0700

    initial commit to remote git

---
+++ MWBarcodeDemo/Resources/ui/common/FirstView.js
@@ -0,0 +1,11 @@
+//FirstView Component Constructor
+function FirstView() {
+	//create object instance, a parasitic subclass of Observable
+	var self = Ti.UI.createView();
+	
+	//label using localization-ready strings from <app dir>/i18n/en/strings.xml
+
+	return self;
+}
+
+module.exports = FirstView;
---
+++ module_android/src/com/manateeworks/barcodescanner/OverlayView.java
@@ -0,0 +1,12 @@
+package com.manateeworks.barcodescanner;
+
+import org.appcelerator.titanium.proxy.TiViewProxy;
+
+public final class OverlayView {
+    public static TiViewProxy overlayProxy = null;
+
+    private OverlayView() {
+
+    }
+
+}
